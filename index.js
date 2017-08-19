3d8817b888a858327ac3a75e324dd65818a4740b
johnhorsema <jjmace01@gmail.com>
2017-08-19 22:40:49 +0800

    Supports external urls.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var Vibrant = require('node-vibrant')
+var Vibrant = require('node-vibrant');
+var getColors = require('get-image-colors');
 
 function generateCircle(color){
 	return '<div style="display: inline-block; border-radius: 5rem; width: 20px; height: 20px; background-color: rgb('+ color +')"></div>';
@@ -13,27 +14,27 @@ function imgTag(path){
 
 app.use('/source', express.static('source'));
 
-app.get('/', function(req, res){
+app.get('/getcolor', function (req, res, next) {
+	var id = req.query.url;
 	var tasks = [];
-	fs.readdir('source', function(err, items) {
-	    for (var i=0; i<items.length; i++) {
-	        var v = new Vibrant('source/'+items[i], {});
-	        tasks.push(v.getPalette());
-	    }
-	    Promise.all(tasks).then(function(palettes){
-			var final_res = palettes.reduce(function(acc, palette, idx){
-				console.log('processing: '+idx+'/'+items.length);
-				var vibrant_color = palette['Vibrant']['_rgb'];
-				var l_vibrant_color = palette['LightVibrant']['_rgb'];
-				var d_vibrant_color = palette['DarkVibrant']['_rgb'];
-				var muted_color = palette['Muted']['_rgb'];
-				var l_muted_color = palette['LightMuted']['_rgb'];
-				var d_muted_color = palette['DarkMuted']['_rgb'];
-				return acc + '<tr><td>'+ imgTag(items[idx]) +'</td><td>' + generateCircle(vibrant_color)+generateCircle(l_vibrant_color)+generateCircle(d_vibrant_color)+generateCircle(muted_color)+generateCircle(l_muted_color)+generateCircle(d_muted_color)+'</td></tr>';
-			},'');
-			res.send('<table><tbody>'+final_res+'</tbody></table>');
-		});
-	});
+	var res_json = {};
+
+	var v = new Vibrant(id, {});
+    tasks.push(v.getPalette());
+    tasks.push(getColors(id));
+
+    Promise.all(tasks).then(function(colors){
+    	var vibrants = Object.keys(colors[0]).map(function(a){
+    		return colors[0][a]['_rgb'];
+    	});
+    	var normals = colors[1].map(function(a){
+    		return a['_rgb'];
+    	});
+    	res.send({
+    		vibrants: vibrants,
+    		normals: normals
+    	});
+    });
 });
 
 app.listen(3000);
